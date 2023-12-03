2ccdb7b772975b3b936df12f16f26786cf1dd823
nicolec2546 <144353597+nicolec2546@users.noreply.github.com>
2023-12-03 19:11:37 +0000

    done

---
+++ diceroll.py
@@ -0,0 +1,28 @@
+#random number for each die and for the computer's number
+
+import random            
+
+die1 = random.randint(1,6);
+die2 = random.randint(1,6);
+diceValue = die1 + die2;
+
+# need input
+
+number = input("Please choose a number between 1 and 12: ")
+computerNumber = random.randint(1,10)
+print ("The computer number is: " + str(computerNumber))
+print ("The result is: " + str(diceValue))
+
+def main():
+    if (diceValue == number):
+        result1 = "Congrats, you won!"
+        return result1
+    elif (diceValue == computerNumber):
+        result2 = "You lost!"
+        return result2
+    else:
+        result3 = "It is a tie!"
+        return result3
+    
+result = main()
+print(result)
